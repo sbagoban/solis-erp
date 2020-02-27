f46d8152335d2d01a1ae0d7d17b3dde374b46ca5
Luisia Louis <>
2020-02-27 10:15:34 +0400

    CORRECTION : Update in SQL alter room_booking

--- sql/2_BOOKING_SYSTEM_ALTER.sql
+++ sql/2_BOOKING_SYSTEM_ALTER.sql
@@ -3,5 +3,4 @@ ADD COLUMN `id_booking` INT(11) NOT NULL DEFAULT 0 COMMENT 'id_booking from book
 ADD KEY `id_bookingfk` (`id_booking`);
 
 ALTER TABLE `dbsolis`.`booking_room_log`
-ADD COLUMN `id_booking` INT(11) NOT NULL DEFAULT 0 COMMENT 'id_booking from booking table' AFTER `id_booking_room`,
-ADD KEY `id_bookingfk` (`id_booking`);
+ADD COLUMN `id_booking` INT(11) NOT NULL DEFAULT 0 COMMENT 'id_booking from booking table' AFTER `id_booking_room`;
