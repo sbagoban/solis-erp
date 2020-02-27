b9e38d93ca1f7e928c1ad7c0d966bfc869e939b3
Luisia Louis <>
2020-02-27 10:11:07 +0400

    NEW : ALTER room_booking_table

---
+++ sql/2_BOOKING_SYSTEM_ALTER.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `dbsolis`.`booking_room`
+ADD COLUMN `id_booking` INT(11) NOT NULL DEFAULT 0 COMMENT 'id_booking from booking table' AFTER `id_booking_room`,
+ADD KEY `id_bookingfk` (`id_booking`);
+
+ALTER TABLE `dbsolis`.`booking_room_log`
+ADD COLUMN `id_booking` INT(11) NOT NULL DEFAULT 0 COMMENT 'id_booking from booking table' AFTER `id_booking_room`,
+ADD KEY `id_bookingfk` (`id_booking`);
