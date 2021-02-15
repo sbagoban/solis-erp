10159713468c49160e6017b069bd9ea8b3f148c3
karvesh <karveshg@gmail.com>
2021-02-15 08:33:40 +0400

    correction of decimal places for meal supplements

--- sql/karvesh db changes/decimals_meals.sql
+++ sql/karvesh db changes/decimals_meals.sql
@@ -10,3 +10,11 @@ CHANGE COLUMN `adult_count` `adult_count` DECIMAL(10,2) NOT NULL DEFAULT '0' ;
 
 ALTER TABLE `dbsolis`.`tblservice_contract_extrasupplement_childages` 
 CHANGE COLUMN `child_count` `child_count` DECIMAL(10,2) NOT NULL DEFAULT '0' ;
+
+
+ALTER TABLE `dbsolis`.`tblspecial_offer_flatrate_mealsupp` 
+CHANGE COLUMN `adult` `adult` DECIMAL(10,2) NULL DEFAULT NULL ;
+
+
+ALTER TABLE `dbsolis`.`tblspecial_offer_flatrate_mealsupp_children_ages` 
+CHANGE COLUMN `child_count` `child_count` DECIMAL(10,2) NOT NULL DEFAULT '0' ;
